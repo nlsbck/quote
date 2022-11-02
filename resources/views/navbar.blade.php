964c9da66605512d891cf0365fa96ee742b8b4d1
nlsbck <nils-bock@t-online.de>
2022-11-02 15:53:47 +0100

    login and logout

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <h4 style="text-align: right; cursor: pointer"><a href=@php(env('WDU'))"/login">Login</a></h4>
+<div class="container">
+    <div class="row">
+        <div class="col"></div>
+        @auth
+            <div class="col text-end">
+                <p style="margin-top: 7px">Logged in as {{auth()->user()->name}}</p>
+            </div>
+            <div class="col-sm-1 text-end">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="btn">Logout</button>
+                </form>
+            </div>
+        @else
+            <div class="col-sm-1 text-end">
+                <h4 style="cursor: pointer">
+                    <a href=@php(env('WDU'))"/login" class="btn">Login</a>
+                </h4>
+            </div>
+        @endauth
+    </div>
 </div>
+
+
